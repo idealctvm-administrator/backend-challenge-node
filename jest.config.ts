615d18a7f82b551c6f9e7442d6b5e035b5ba375f
vincenzo <vincenzo@DESKTOP-HS1D480.localdomain>
2023-04-01 08:38:14 -0300

    chore: add dependecies to the project

---
+++ jest.config.ts
@@ -0,0 +1,33 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: 'coverage',
+
+  // An array of regexp pattern strings used to skip coverage collection
+  coveragePathIgnorePatterns: ['/node_modules/'],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: 'v8',
+
+  // A list of paths to directories that Jest should use to search for files in
+  roots: ['<rootDir>/tests'],
+
+  // The test environment that will be used for testing
+  testEnvironment: 'node',
+
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+};
